7ad0031ff04cee8e9e145145126bcbd2965bc51b
manyabisht <imanya1007@gmail.com>
2024-12-29 20:21:49 +0530

    updated

---
+++ server.js
@@ -0,0 +1,11 @@
+// Dev.0/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
